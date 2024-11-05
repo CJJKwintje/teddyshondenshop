da5321a49c9d88bc5c6c82d4f5b0244cebbfe929
CJJKwintje <christiaan.kwint@icloud.com>
2024-11-05 21:35:28 +0100

    Redirects

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: ''
+        }
+      ]
+    })
+  ],
   optimizeDeps: {
     include: ['@urql/core', 'urql'],
   },
   build: {
     commonjsOptions: {
       include: [/node_modules/],
-      transformMixedEsModules: true
-    }
-  }
+      transformMixedEsModules: true,
+    },
+  },
 });
